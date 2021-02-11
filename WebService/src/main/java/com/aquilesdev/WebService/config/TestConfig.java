b9992859d8326862c82475afe88900bc061d8de0
AquilesDias <kilesax@gmail.com>
2021-02-11 14:16:55 -0300

    Category

--- WebService/src/main/java/com/aquilesdev/WebService/config/TestConfig.java
+++ WebService/src/main/java/com/aquilesdev/WebService/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.aquilesdev.WebService.entities.Category;
 import com.aquilesdev.WebService.entities.Order;
 import com.aquilesdev.WebService.entities.User;
 import com.aquilesdev.WebService.entities.enums.OrderStatus;
+import com.aquilesdev.WebService.repository.CategoryRepository;
 import com.aquilesdev.WebService.repository.OrderRepository;
 import com.aquilesdev.WebService.repository.UserRepository;
 
@@ -20,6 +22,9 @@ public class TestConfig implements CommandLineRunner{
 	 
 	 @Autowired
      private UserRepository userRepository;
+	 
+	 @Autowired
+	 private CategoryRepository categoryRepository;
 	 
 	 @Autowired
 	 private OrderRepository orderRepository;
@@ -34,9 +39,15 @@ public class TestConfig implements CommandLineRunner{
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.WAITING_PAYMENT, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.WAITING_PAYMENT, u1);
 		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+		
 		userRepository.saveAll(Arrays.asList(u1,u2));
 		
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 	}
 	 
 	 
