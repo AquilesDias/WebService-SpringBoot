cecda400f24a0c813a9e0edd0d86c2fa7935f2fe
AquilesDias <kilesax@gmail.com>
2021-02-16 11:35:47 -0300

    Exception handling - findById

--- WebService/src/main/java/com/aquilesdev/WebService/service/UserService.java
+++ WebService/src/main/java/com/aquilesdev/WebService/service/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.aquilesdev.WebService.entities.User;
 import com.aquilesdev.WebService.repository.UserRepository;
+import com.aquilesdev.WebService.service.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 	  Optional<User> obj = userRepository.findById(id);
-	  return obj.get();
+	  return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
