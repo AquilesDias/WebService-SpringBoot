3f6f78de9d8d0930630579a263a8d8b3b1046e97
AquilesDias <kilesax@gmail.com>
2021-02-17 08:06:07 -0300

    User update

--- WebService/src/main/java/com/aquilesdev/WebService/resources/exceptions/ResourceExceptionHandler.java
+++ WebService/src/main/java/com/aquilesdev/WebService/resources/exceptions/ResourceExceptionHandler.java
@@ -22,7 +22,7 @@ public class ResourceExceptionHandler {
 		HttpStatus status = HttpStatus.NOT_FOUND;
 		StandardError err  = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
-	}
+	} 
 	
 	@ExceptionHandler(DatabaseException.class)
 	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request){
